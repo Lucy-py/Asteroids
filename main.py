1671c3313553989ce1c723c54a37009f0f886108
Lucy-py <marvinkoschine.mk@gmail.com>
2026-06-25 10:20:03 +0200

    added circleshape.py

--- main.py
+++ main.py
@@ -11,7 +11,13 @@ from logger import log_state
 
 def main():
     pygame.init()
+    
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    clock = pygame.time.Clock()
+    dt = 0.0
+
+    # vv Gameloop vv
     while True:
         log_state()
         for event in pygame.event.get():
@@ -20,6 +26,8 @@ def main():
 
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+    
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
