fb1693b54310ffba0c826acc0a080368bd481673
Lucy-py <marvinkoschine.mk@gmail.com>
2026-06-26 08:02:59 +0200

    Implement shot collision detection with asteroids

--- main.py
+++ main.py
@@ -52,6 +52,11 @@ def main():
                 log_event("player_hit")
                 print("Game over!")
                 sys.exit()
+            for shot in shots:
+                if shot.collides_with(asteroid):
+                    log_event("asteroid_shot")
+                    asteroid.kill()
+                    shot.kill()
 
         for draw in drawable:
             draw.draw(screen)
